9ad9beb842999ad76b8a7cc5a496c325d30c38f9
Shruthi <shruthimarri97@gmail.com>
2021-05-04 15:01:25 +0530

    [Shruthi] : Added Simple LinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListNode list = new LinkedListNode();
+            list.InsertLast(56);
+            list.InsertLast(30);
+            list.InsertLast(70);
+            list.Display();
+            Console.ReadLine();
+        }
+    }
+}
